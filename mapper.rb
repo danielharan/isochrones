ed9808a71c95984f334f5335ea16695f650aab12
Daniel Haran <chebuctonian@mgmail.com>
2008-11-09 16:04:09 -0500

    Breath-first algo we're trying to implement

--- mapper.rb
+++ mapper.rb
@@ -4,10 +4,11 @@ require 'fastercsv'
 class Mapper
   FEED_DIR = 'sample-feed'
   
-  attr_accessor :trips, :stop_times
+  attr_accessor :trips, :stop_times, :best_times
   def initialize
     load_trips
     load_stop_times
+    @best_times = {}
   end
   
   def load_trips
@@ -17,8 +18,25 @@ class Mapper
   def load_stop_times
     @stop_times = FasterCSV.read("#{FEED_DIR}/stop_times.txt").to_a[1..-1]
   end
+  
+  def isochrone(stop, time)
+    @best_times[stop] = time
+    @stack = [stop]
+    while !@stack.empty?
+      traverse(stop.pop, time)
+    end
+  end
+  
+  private
+    def traverse(stop,time)
+      stop.available_hops.each do |hop|
+        if @best_times[hop.destination_stop] > hop.end_time
+          @best_times[hop.destination_stop] = hop.end_time
+          @stack << hop.destination_stop
+        end
+      end
+    end
 end
-
 
 class FeedObject
   def self.load(path)
