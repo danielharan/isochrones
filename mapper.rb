a3cef3f023058382aa740f99ccac342d39db2485
Daniel Haran <chebuctonian@mgmail.com>
2008-11-15 13:54:50 -0500

    implement stop.available_hops

--- mapper.rb
+++ mapper.rb
@@ -1,11 +1,15 @@
 require 'rubygems'
 require 'fastercsv'
 require 'ostruct'
+require 'active_support'
 
 class Mapper
   attr_accessor :trips, :stop_times, :stops, :best_times
   def initialize(feed_dir)
     load_data(feed_dir)
+    
+    # structure the graph data in a way that we can use
+    Stop.generate_hops(@stops, @stop_times)
   end
   
   def load_data(feed_dir)
@@ -41,4 +45,32 @@ end
 
 class Trip     < FeedObject; end
 class StopTime < FeedObject; end
-class Stop     < FeedObject; end
+
+class Hop < OpenStruct
+  def initialize(from,to)
+    super :departure_time => from.departure_time, :arrival_time => to.arrival_time,
+          :trip_id => from.trip_id, :destination => to.stop_id
+  end
+end
+
+class Stop     < FeedObject
+  attr_accessor :available_hops
+  
+  def initialize(args)
+    super(args)
+    @available_hops = []
+  end
+  
+  def self.generate_hops(stops,stop_times)
+    stops = stops.inject({}) {|memo,stop| memo[stop.stop_id] = stop; memo }
+    
+    stop_times.group_by {|st| st.trip_id}.each do |trip_id,trip_stops|
+      trip_stops = trip_stops.sort_by(&:stop_sequence)
+      
+      (by_pair = trip_stops.zip(trip_stops[1..-1])).pop
+      by_pair.each do |from,to|
+        stops[from.stop_id].available_hops << Hop.new(from,to)
+      end
+    end
+  end
+end
--- mapper_test.rb
+++ mapper_test.rb
@@ -1,7 +1,7 @@
 require 'test/unit'
 require 'mapper'
 require 'rubygems'
-
+require 'activesupport'
 
 class MapperTest < Test::Unit::TestCase
   
@@ -10,6 +10,18 @@ class MapperTest < Test::Unit::TestCase
     assert_not_nil m.trips
     assert_not_nil m.stop_times
     assert_not_nil m.stops
+  end
+  
+  def test_hop_creation
+    m = Mapper.new('sample-feed')
+    stagecoach_stop = m.stops.detect {|stop| stop.stop_id == "STAGECOACH"}
+    assert_equal 2, stagecoach_stop.available_hops.length
+    
+    stba_hop = stagecoach_stop.available_hops.detect {|hop| hop.trip_id == "STBA"}
+    assert_equal "BEATTY_AIRPORT", stba_hop.destination
+    assert_equal "6:20:00",        stba_hop.arrival_time
+    
+    assert_equal "NANAA", stagecoach_stop.available_hops.detect {|hop| hop.trip_id == "CITY1"}.destination
   end
 end
 
@@ -27,5 +39,19 @@ class StopTimeTest < Test::Unit::TestCase
     stop_time = StopTime.load("sample-feed/stop_times.txt").first
     
     assert_equal 'STBA', stop_time.trip_id
+  end
+end
+
+class HopTest < Test::Unit::TestCase
+  def test_initialize
+    from = StopTime.new :stop_sequence => 1, :arrival_time => "6:00:00", :departure_time => "6:01:00",
+                        :stop_id => "STAGECOACH", :trip_id => "STBA"
+    to   = StopTime.new :stop_sequence => 2, :arrival_time => "6:20:00", :departure_time => "6:21:00",
+                        :stop_id => "BEATTY_AIRPORT", :trip_id => "STBA"
+    hop = Hop.new(from,to)
+    assert_equal "6:01:00",        hop.departure_time
+    assert_equal "6:20:00",        hop.arrival_time
+    assert_equal "STBA",           hop.trip_id
+    assert_equal "BEATTY_AIRPORT", hop.destination
   end
 end
